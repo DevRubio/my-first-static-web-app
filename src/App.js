bd081a0c4b56746d1b4a2cc451a130f6eb74f6c7
Oscar Rubio <oscar.franco@softwareone.com>
2025-01-22 07:33:23 -0500

    feat: fetch and display data from API in App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(()=>{
+    (async function(){
+      const response = await (await fetch(`api/disponible/read`)).json()
+      setData(response)
+    })
+  })
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {response}</div>;
 }
 
 export default App;
